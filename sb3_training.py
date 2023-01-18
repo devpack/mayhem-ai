eb33eaa0e629418255f1818b89512000dd3a6451
devpack <anthony.prieur@gmail.com>
2023-01-18 16:49:10 +0100

    Cleanup SB3 training

--- sb3_training.py
+++ sb3_training.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 """
+- Train with PPO or A2C for 1_000_000 steps and save model every 10_000 step
 
+python3 sb3_training.py --algo=PPO --timesteps=1_000_000 --save_every=10_000
+python3 sb3_training.py --algo=A2C --timesteps=1_000_000 --save_every=10_000
+
+- Load trained model:
+
+python3 sb3_training.py --algo=PPO --model_name=40000.zip
+python3 sb3_training.py --algo=A2C --model_name=20000.zip
+
+- TensorrBoard: http://localhost:6006/
+
+tensorboard --logdir=logs
 """
 
 import argparse, sys
@@ -9,10 +21,15 @@ from stable_baselines3 import A2C, PPO
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.callbacks import BaseCallback
 
 from mayhem import *
 
-MODE = "training"
+# -------------------------------------------------------------------------------------------------
+
+DETERMINISTIC     = False
+MAX_FRAME_TO_PLAY = 30000
+NB_TEST_RUN       = 10
 
 # -------------------------------------------------------------------------------------------------
 
@@ -21,129 +38,102 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-width', '--width', help='', type=int, action="store", default=1200)
 parser.add_argument('-height', '--height', help='', type=int, action="store", default=800)
 
+parser.add_argument('-algo', '--algo', help='', action="store", default="PPO", choices=("PPO", "A2C"))
+parser.add_argument('-model_name', '--model_name', help='', action="store", default=None)
+
+parser.add_argument('-timesteps', '--timesteps', help='', type=int, action="store", default=2_000_000)
+parser.add_argument('-save_every', '--save_every', help='', type=int, action="store", default=20_000)
+
+parser.add_argument('-log_dir', '--log_dir', help='', action="store", default="logs")
+
 result = parser.parse_args()
 args = dict(result._get_kwargs())
 
-print("Args", args)
+print("Args=", args)
 
 # -------------------------------------------------------------------------------------------------
 
 init_pygame()
 
-game_window = GameWindow(args["width"], args["height"], MODE, debug_on_screen=0)
+game_window = GameWindow(args["width"], args["height"], "training", debug_on_screen=0)
 
 env = MayhemEnv(game_window, 
                 vsync=False, 
                 render_game=False, 
                 nb_player=1, 
-                mode=MODE, 
+                mode="training", 
                 motion="gravity",
                 sensor="ray", 
                 record_play=False, 
                 play_recorded=False)
-                
+
+check_env(env)
+
 # -------------------------------------------------------------------------------------------------
 
-#env = DummyVecEnv([lambda: env])
-#env = make_vec_env(env, n_envs=10)
-if 1:
-    check_env(env)
+class CustomSaveCallback(BaseCallback):
 
-#print("action space=", env.action_space)
-#print("obs space=", env.observation_space)
+    def __init__(self, save_freq=10000, logdir="./logs/", model_prefix="PPO", verbose=1):
+        super(CustomSaveCallback, self).__init__(verbose)
+        
+        self.save_freq = save_freq
+        self.logdir = logdir
+        self.save_path = os.path.join(self.logdir, "models", model_prefix)
 
-#from gym.wrappers.normalize import NormalizeObservation
-#wrapped_env = NormalizeObservation(env)
-#print("wrapped obs space=", wrapped_env.observation_space)
+        if not os.path.exists(self.logdir):
+            os.makedirs(self.logdir)
 
-#sys.exit(0)
+        if not os.path.exists(self.save_path):
+            os.makedirs(self.save_path)
 
-#model_type = "A2C"
-model_type = "PPO"
+    def _on_step(self) -> bool:
 
-# load
-LOAD_MODEL_NAME = None  
-#LOAD_MODEL_NAME = "1540000.zip"    
-DETERMINISTIC = 0
-MAX_FRAME = 20000
-EPISODES_PLAY = 5
+        if self.n_calls % self.save_freq == 0:
 
-# train
-TIMESTEPS = 20000
-EPISODES_TRAIN = 100
+            if self.verbose > 0:
+                print(f"=> Saving {self.save_path}/{self.num_timesteps}.zip")
 
-if not LOAD_MODEL_NAME:
+            self.model.save( f"{self.save_path}/{self.num_timesteps}" )
+
+        return True
+
+
+# -------------------------------------------------------------------------------------------------
+
+if not args["model_name"]:
     pygame.display.iconify()
-
-models_dir = "models/%s" % model_type
-logdir = "logs"
-
-if not os.path.exists(models_dir):
-    os.makedirs(models_dir)
-
-if not os.path.exists(logdir):
-    os.makedirs(logdir)
 
 obs = env.reset()
 
-# --- load
-if LOAD_MODEL_NAME:
-    model_path = f"{models_dir}/%s" % LOAD_MODEL_NAME
+# -------------------------------------------------------------------------------------------------
 
-    if model_type == "A2C":
+# --- train
+if not args["model_name"]:
+
+    if args["algo"] == "A2C":
+        model = A2C('MlpPolicy', env, verbose=1, tensorboard_log=args["log_dir"])
+    elif args["algo"] == "PPO":
+        model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=args["log_dir"])
+
+    save_cb = CustomSaveCallback(save_freq=args["save_every"], logdir=args["log_dir"], model_prefix=args["algo"], verbose=1)
+
+    #model.learn(total_timesteps=args["timesteps"], reset_num_timesteps=False, tb_log_name=args["algo"])
+    model.learn(total_timesteps=args["timesteps"], tb_log_name=args["algo"], callback=[save_cb, ])
+
+# --- load
+else:
+    model_path = os.path.join(args["log_dir"], "models", args["algo"], args["model_name"])
+
+    if args["algo"] == "A2C":
         model = A2C.load(model_path, env=env)
-    elif model_type == "PPO":
+    elif args["algo"] == "PPO":
         model = PPO.load(model_path, env=env)
 
-    for ep in range(EPISODES_PLAY):
+    for ep in range(NB_TEST_RUN):
         obs = env.reset()
         done = False
 
         while not done:
             action, _states = model.predict(obs, deterministic=DETERMINISTIC)
-            obs, rewards, done, info = env.step(action, max_frame=MAX_FRAME)
+            obs, rewards, done, info = env.step(action, max_frame=MAX_FRAME_TO_PLAY)
             env.render(collision_check=False)
-
-# --- train
-else:
-    if model_type == "A2C":
-        model = A2C('MlpPolicy', env, verbose=1, tensorboard_log=logdir)
-    elif model_type == "PPO":
-        model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=logdir)
-
-    #iters = 0
-    for i in range(EPISODES_TRAIN):
-        model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name=model_type)
-        model.save(f"{models_dir}/{TIMESTEPS*i}")
-
-    # train and play trained
-    if 0:
-        model.learn(total_timesteps=100000)
-
-        episodes = 5
-
-        for ep in range(episodes):
-            obs = env.reset()
-            done = False
-            
-            while not done:
-                action, _states = model.predict(obs)
-                obs, rewards, done, info = env.step(action)
-                env.render(collision_check=False)
-                #print(rewards)
-
-# test random moves
-if 0:
-    env.reset()
-
-    print("sample action:", env.action_space.sample())
-    print("observation space shape:", env.observation_space.shape)
-    print("sample observation:", env.observation_space.sample())
-
-    done = False
-
-    while not done:
-        new_state, reward, done, info = env.step(env.action_space.sample())
-        env.render(collision_check=True)
-
-
